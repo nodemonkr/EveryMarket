aad1a148a796ec7da5ecba19efd519a772067f67
joon <jackma914@gmail.com>
2021-06-30 17:29:33 +0900

    ADD
    create-react-app

--- joon/src/App.js
+++ joon/src/App.js
@@ -1,39 +1,24 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
-import Section from "./components/Section";
-import Sidebar from "./components/Sidebar";
-import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import Login from "./pages/Login";
-import Upload from "./pages/Upload";
-import Live from "./pages/Live";
-import { db } from "./firebase";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  // 유튜브 초기화면 포스트글들
-  // const [posts, setPosts] = useState([]);
-
-  // useEffect(() => {
-  //   db.collection("posts").onSnapshot((snapshot) => {
-  //     setPosts(snapshot.docs.map((doc) => doc.data()));
-  //   });
-  // }, []);
-
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/upload" component={Upload} />
-          <Route path="/live" component={Live} />
-          <Route>
-            <Header />
-            <Sidebar />
-            <Section />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
