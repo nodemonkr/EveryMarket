6c7bb0ec241facf3ae1e26647ba877ce27c89dee
joon <jackma914@gmail.com>
2021-07-07 19:52:13 +0900

    2021_07_07 joon업데이트
    ADD: login, signup 화면

--- joon/client/src/App.js
+++ joon/client/src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import React from "react";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+//Pages
+import Home from "./pages/Home";
+//styled components
+import { StyledContainer } from "./components/Styles";
+
+//loader css
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <StyledContainer>
+        {/* <Login /> */}
+        <Signup />
+      </StyledContainer>
+    </Router>
+  );
 }
 
 export default App;
