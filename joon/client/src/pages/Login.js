e535f43408fd4870ab029efa1f1de7c076a22c90
joon <jackma914@gmail.com>
2021-07-13 02:20:13 +0900

    2021_07_13 ADD
    리액트에서 노드로 이메일, 비밀번호 까지는 보내졌다

--- joon/client/src/pages/Login.js
+++ joon/client/src/pages/Login.js
@@ -1,3 +1,6 @@
+// 서버전송용 라이브러리
+import Axios from "axios";
+
 //styled components
 import {
   StyledTextInput,
@@ -29,7 +32,25 @@ import Loader from "react-loader-spinner";
 import { connect } from "react-redux";
 import { loginUser } from "../auth/actions/userActions";
 import { useHistory } from "react-router-dom";
+//import { response } from "express";
 
+//로그인함수구현
+const loginbutton = () => {
+  Axios.post("http://127.0.0.1:3100/login", {
+    email: "testemail",
+    password: "testpasswd",
+  }).then((res) => {
+    console.log(res);
+  });
+};
+// .then(function (response) {
+//   console.log(response);
+//   //응답에 따른 처리
+// })
+// .catch(function (error) {
+//   console.log(error);
+//   // error에따른 처리
+// })
 const Login = (loginUser) => {
   const history = useHistory();
   return (
@@ -52,9 +73,41 @@ const Login = (loginUser) => {
               .max(30, "Password is too long")
               .required("Required"),
           })}
+          // server쪽으로 보내는 함수
           onSubmit={(values, { setSubmitting, setFieldError }) => {
-            console.log(values);
-            loginUser(values, history, setFieldError, setSubmitting);
+            console.log(
+              "콘솔용 이메일값 : " +
+                values.email +
+                "콘솔용 비밀번호" +
+                values.password
+            ); // 값테스트용 (정상)
+            // 서버쪽 데이터전송(테스트용 데이터 송신)
+            var email = values.email;
+            // // fetch test
+            // fetch("http://127.0.0.1:3100/login", {
+            //   method: "POST",
+            //   body: JSON.parse(email),
+            //   headers: {
+            //     "content-Type": "application/json",
+            //   },
+            //});
+
+            Axios.post({
+              url: "http://127.0.0.1:3100/login",
+              data: {
+                email: values.email,
+                password: values.password,
+              }
+                .then(function (response) {
+                  console.log(response);
+                  //응답에 따른 처리
+                })
+                .catch(function (error) {
+                  console.log(error);
+                  // error에따른 처리
+                }),
+              //loginUser(values, history, setFieldError, setSubmitting);
+            });
           }}
         >
           {/* ---login 버튼 클릭 로딩 구현 start ---*/}
@@ -64,6 +117,7 @@ const Login = (loginUser) => {
                 name="email"
                 type="text"
                 label="Email"
+                id="email"
                 placeholder="test1@example.com"
                 icon={<FiMail />}
               />
@@ -71,12 +125,16 @@ const Login = (loginUser) => {
                 name="password"
                 type="password"
                 label="Password"
+                id="pw"
                 placeholder="******"
                 icon={<FiLock />}
               />
               <ButtonGroup>
                 {!isSubmitting && (
-                  <StyledFormButton type="submit">Login</StyledFormButton>
+                  //<StyledFormButton type="submit" onClick={this.loginbutton()}>
+                  <StyledFormButton onClick={loginbutton()}>
+                    Login
+                  </StyledFormButton>
                 )}
 
                 {isSubmitting && (
--- joon/server/index.js
+++ joon/server/index.js
@@ -1,30 +1,44 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
-const post = 3000;
+const bodyParsers = require("body-parser");
+// 서버포트
+const post = 3100;
 const db = require("../server/config/db");
 const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
+app.use(bodyParsers.json());
+app.use(bodyParsers.urlencoded({ extended: false }));
+
 // connect to our database
 db.connect((err) => {
   if (err) throw err;
 });
 
-app.get("/", (req, res) => {
-  db.query(
-    "INSERT INTO login(email, password) VALUES(?,?)",
-    [email, password],
-    (err, result) => {
-      console.log(err);
-    }
-  );
+//로그인
+app.post("/login", (req, res) => {
+  console.log(req.body.email); //아이디와 비밀번호가 정상적으로 수신됬는지 확인
+  // 입력받은 id, pw 변수화처리 (원래는 이 변수를 이용해서 비밀번호 암호화 필수)
+  // var tmp_email = req.email;
+  // var tmp_pw = req.password;
+  // console.log("로그인 처리에 필요한데이터 :" + tmp_email + tmp_pw);
+  // db.query(
+  //   "INSERT INTO login(email, password) VALUES(?,?)",
+  //   [email, password],
+  //   (err, result) => {
+  //     console.log(err);
+  //   }
+  // );
 });
+
+//login은 post로 받아서 처리할것 (값을 요청하는것이아닌 로그인을 db에 요청하는것)
+// passport 추후에 구성할것
 
 // set app port
 
-app.listen(port, () => {
+app.listen(post, () => {
   console.log(`listening at http://localhost:${post}`);
 });
